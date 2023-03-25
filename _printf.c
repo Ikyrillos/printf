9091bd267752b5d5e186c16fe470ba499d156d3f
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-03-25 21:55:20 +0200

    recreate _printf

--- _printf.c
+++ _printf.c
@@ -1,48 +1,51 @@
-#include "main.h"
+int _printf(const char * const format, ...)
 
-/**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
- * Authors: Ehoneah Obed & Abdulhakeem Badejo
- */
-int _printf(const char *format, ...)
+/* Implementation of the _printf function that takes a format string and variable number of arguments */
+/* The format string specifies the format of the output string, with placeholders marked by conversion specifiers */
+/* Returns the number of characters printed, or -1 if an error occurs */
+
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+    /* Definition of a struct array that maps conversion specifiers to their corresponding function */
+    convert_match m[] = {
+        {"%s", printf_string}, {"%c", printf_char},
+        {"%%", printf_37},
+        {"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+        {"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+        {"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+        {"%S", printf_exclusive_string}, {"%p", printf_pointer}
+    };
 
-	register int count = 0;
+    va_list args;
+    int i = 0, j, len = 0;
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    va_start(args, format);
+
+    /* Check if the format string is null or empty */
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
+
+Here:
+    /* Loop through the format string and look for conversion specifiers */
+    while (format[i] != '\0')
+    {
+        /* Loop through the array of conversion specifiers and call the corresponding function */
+        j = 13;
+        while (j >= 0)
+        {
+            if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+            {
+                len += m[j].f(args);
+                i = i + 2;
+                goto Here;
+            }
+            j--;
+        }
+        /* If no conversion specifier is found, print the character */
+        _putchar(format[i]);
+        len++;
+        i++;
+    }
+
+    va_end(args);
+    return (len);
 }
