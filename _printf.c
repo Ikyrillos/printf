992b0b2eeaeaedf61f9e601f2354b0c164de5ed9
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-03-25 21:47:42 +0200

    _printf

--- _printf.c
+++ _printf.c
@@ -1,57 +1,48 @@
 #include "main.h"
 
 /**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - A function that prints formatted output to stdout
- * @format: The format string.
- *
- * Return: The number of characters printed, or -1 if an error occurs.
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ * Authors: Ehoneah Obed & Abdulhakeem Badejo
  */
 int _printf(const char *format, ...)
 {
-    va_list list;
-    va_start(list, format);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-    char buffer[BUFF_SIZE];
-    int buff_ind = 0, printed_chars = 0;
+	register int count = 0;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format = handle_print(format, &buff_ind, list, buffer);
-            if (format == NULL)
-                return -1;
-        }
-        else
-        {
-            buffer[buff_ind++] = *format++;
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
-            printed_chars++;
-        }
-    }
-
-    print_buffer(buffer, &buff_ind);
-
-    va_end(list);
-    return printed_chars;
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-    if (*buff_ind > 0)
-        write(1, buffer, *buff_ind);
-    *buff_ind = 0;
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
